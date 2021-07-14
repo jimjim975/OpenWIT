59ca6bc1105fd5ba2e45c4a9d3e3d97b579a00df
jimjim975 <jim@rjgdesign.com>
2021-07-14 01:34:41 -0400

    Updated to final database table info

--- tableCreate.sql
+++ tableCreate.sql
@@ -1,42 +1,42 @@
 USE [openwitprod]
 GO
 
-/****** Object:  Table [dbo].[ProdTable]    Script Date: 6/17/2021 10:02:29 PM ******/
+/****** Object:  Table [dbo].[ProdTableNew]    Script Date: 7/14/2021 1:34:13 AM ******/
 SET ANSI_NULLS ON
 GO
 
 SET QUOTED_IDENTIFIER ON
 GO
 
-CREATE TABLE [dbo].[ProdTable](
-	[timestamp] [nchar](90) NOT NULL,
-	[age] [nchar](30) NOT NULL,
-	[gender] [nchar](30) NOT NULL,
-	[gradyear] [nchar](30) NOT NULL,
-	[major] [nchar](30) NULL,
-	[minor] [nchar](30) NULL,
-	[withdrawals] [nchar](30) NULL,
-	[collegechoice] [nchar](30) NULL,
-	[campusliving] [nchar](30) NULL,
-	[exercise] [nchar](30) NULL,
-	[employed] [nchar](30) NULL,
-	[coops] [nchar](30) NULL,
-	[extracurricular] [nchar](30) NULL,
-	[classnum] [nchar](30) NULL,
-	[timeoutfriends] [nchar](30) NULL,
-	[vaccinated] [nchar](30) NULL,
-	[classesfailed] [nchar](30) NULL,
-	[freetime] [nchar](30) NULL,
-	[druguse] [nchar](30) NULL,
-	[cwentworthr] [nchar](30) NULL,
-	[hstudying] [nchar](30) NULL,
+CREATE TABLE [dbo].[ProdTableNew](
+	[timestamp] [nvarchar](90) NOT NULL,
+	[age] [nvarchar](30) NOT NULL,
+	[gender] [nvarchar](30) NOT NULL,
+	[gradyear] [nvarchar](30) NOT NULL,
+	[major] [nvarchar](30) NULL,
+	[minor] [nvarchar](30) NULL,
+	[withdrawals] [nvarchar](30) NULL,
+	[collegechoice] [nvarchar](30) NULL,
+	[campusliving] [nvarchar](30) NULL,
+	[exercise] [nvarchar](30) NULL,
+	[employed] [nvarchar](30) NULL,
+	[coops] [nvarchar](30) NULL,
+	[extracurricular] [nvarchar](30) NULL,
+	[classnum] [nvarchar](30) NULL,
+	[timeoutfriends] [nvarchar](30) NULL,
+	[vaccinated] [nvarchar](30) NULL,
+	[classesfailed] [nvarchar](30) NULL,
+	[freetime] [nvarchar](30) NULL,
+	[druguse] [nvarchar](30) NULL,
+	[cwentworthr] [nvarchar](30) NULL,
+	[hstudying] [nvarchar](30) NULL,
 	[GPA] [numeric](3, 2) NULL,
-	[overallexp] [nchar](30) NULL,
-	[mentalhealth] [nchar](30) NULL,
-	[wentresources] [nchar](30) NULL,
-	[firstgen] [nchar](30) NULL,
-	[terms] [nchar](10) NULL,
-	[sleep] [nchar](30) NULL
+	[overallexp] [nvarchar](30) NULL,
+	[mentalhealth] [nvarchar](30) NULL,
+	[wentresources] [nvarchar](30) NULL,
+	[firstgen] [nvarchar](30) NULL,
+	[terms] [nvarchar](10) NULL,
+	[sleep] [nvarchar](30) NULL
 ) ON [PRIMARY]
 GO
 
